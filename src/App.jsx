8d1525473e4a8b025ae9f46a7f88ecb68acebcf2
Nikita-1710 <chausalkarnikita@gmail.com>
2025-05-30 15:37:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css"
+function App() {
+  return (
+    <div>
+      <h1 className="text-blue-500 text-4xl py-5 text-center">React App using Vite</h1>
+    </div>
+  )
+}
+export default App
