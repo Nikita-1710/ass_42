d12953635543d861799a80980915a688c569025e
Nikita-1710 <chausalkarnikita@gmail.com>
2025-05-30 16:25:50 +0530

    text color changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1 className="text-blue-500 text-4xl py-5 text-center font-bold">
-        Ahilyanagar - The City of Fort
+        Welcome to Ahilyanagar
       </h1>
       <div className="flex flex-row w-[1300px] mx-auto my-6">
         <div className="min-h-auto w-1/2">
@@ -16,8 +16,8 @@ function App() {
 
         <div className="min-h-auto w-1/2 flex flex-col my-18">
           <h1 className='text-2xl font-bold text-fuchsia-700 text-center'> Ahilyanagar (Ahmednagar)</h1>
-          <p className='my-6 text-center justify-center'>
-            Ahmednagar, officially Ahilyanagar, is a district in Maharashtra, India, known as the "Place of Saints". It's located between Pune and Aurangabad and is the largest district in Maharashtra by area. The district is known for its strong co-operative sector, including sugar factories and industrial estates. Ahmednagar is also historically significant, serving as the seat of the Ahmednagar Sultanate
+          <p className='my-6 text-[18px] text-center justify-center'>
+            <span className='text-violet-500'>Ahilyanagar</span> is a district in Maharashtra, India, known as the "Place of Saints". It's located between Pune and Aurangabad and is the largest district in Maharashtra by area. The district is known for its strong co-operative sector, including sugar factories and industrial estates. Ahmednagar is also historically significant, serving as the seat of the Ahmednagar Sultanate
           </p>
         </div>
       </div>
